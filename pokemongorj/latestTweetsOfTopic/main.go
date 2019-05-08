10dd404a58d48d15c407bbc9011ed9580704aea5
Arthur Silva <arxdsilva@gmail.com>
2019-05-08 19:06:23 -0300

    stream demux

--- pokemongorj/latestTweetsOfTopic/main.go
+++ pokemongorj/latestTweetsOfTopic/main.go
@@ -10,16 +10,45 @@ import (
 func main() {
 	creds := config.GetCredentials()
 	c, _ := config.GetClient(&creds)
-	params := &twitter.StreamFilterParams{
-		Track:         []string{"pokemon go"},
-		Language:      []string{"pt"},
-		StallWarnings: twitter.Bool(true),
-	}
-	stream, _ := c.Streams.Filter(params)
-	for message := range stream.Messages {
+	streamDemux(c)
+	// params := &twitter.StreamFilterParams{
+	// 	Track:         []string{"pokemon go"},
+	// 	Language:      []string{"pt"},
+	// 	StallWarnings: twitter.Bool(true),
+	// }
+	// stream, _ := c.Streams.Filter(params)
+	// likeStream(stream, c)
+}
+
+func likeStream(s *twitter.Stream, c *twitter.Client) {
+	for message := range s.Messages {
 		t := message.(*twitter.Tweet)
 		p := &twitter.StatusShowParams{TweetMode: "extended"}
 		tweet, _, err := c.Statuses.Show(t.ID, p)
-		fmt.Println(tweet.FullText, err)
+		fmt.Println(tweet.FullText, "\n")
+		tt, _, err := c.Favorites.Create(&twitter.FavoriteCreateParams{ID: t.ID})
+		fmt.Println(tt.ID, err, "\n")
 	}
+}
+
+func streamDemux(c *twitter.Client) {
+	params := &twitter.StreamFilterParams{
+		Track:         []string{"pokemon go"},
+		Language:      []string{"en"},
+		StallWarnings: twitter.Bool(true),
+	}
+	s, err := c.Streams.Filter(params)
+	fmt.Println("Starting Stream...", err)
+	demux := twitter.NewSwitchDemux()
+	demux.Tweet = func(tweet *twitter.Tweet) {
+		p := &twitter.StatusShowParams{TweetMode: "extended"}
+		tweet, _, err := c.Statuses.Show(tweet.ID, p)
+		fmt.Println(tweet.FullText, "\n")
+		tt, _, err := c.Favorites.Create(&twitter.FavoriteCreateParams{ID: tweet.ID})
+		fmt.Println(tt.ID, err, "\n")
+	}
+	demux.HandleChan(s.Messages)
+	fmt.Println("Stopping Stream...")
+	s.Stop()
+	streamDemux(c)
 }
