8f10ab7e464ae52e74598369d087e9f50f88bf90
Arthur Silva <arxdsilva@gmail.com>
2019-06-16 13:13:07 -0300

    remocao simplificada

--- cs/remove.cs
+++ cs/remove.cs
@@ -1,41 +1,42 @@
-
 void AVLTree::Delete(int x, TreePointer &p, bool &h) {
-    TreePointer q;
-    if (p==NULL) {
+    if (p == NULL) {
         count << "Elemento inexistente";
         abort();
     }
     if (x < p->Entry) {
         Delete(x, p->LeftNode, h);
-        if (h) {balenceL(p, h);}
-    } else {
-        if (x > p->Entry) {
-            Delete(x, p->RightNode, h);
-            if (h) {balanceR(p, h);}
-        } else {
-            q = p;
-            if (q->RightNode == NULL) {
-                p = q->LeftNode;
-                h = true;
-            } else {
-                if (q->LeftNode == NULL) {
-                    p = q->RightNode;
-                    h = true;
-                } else {
-                    DelMin(q, q->RightNode, h);
-                    if (h) {balanceR(p, h);}
-                }
-                delete q;
-            }
-        }
+        if (h) { balenceL(p, h); }
+        return;
+    } 
+    if (x > p->Entry) {
+        Delete(x, p->RightNode, h);
+        if (h) { balanceR(p, h); }
+        return;
     }
+    TreePointer q;
+    q = p;
+    if (q->RightNode == NULL) {
+        p = q->LeftNode;
+        h = true;
+        delete q;
+        return;
+    } 
+    if (q->LeftNode == NULL) {
+        p = q->RightNode;
+        h = true;
+    } else {
+        DelMin(q, q->RightNode, h);
+        if (h) { balanceR(p, h); }
+    }
+    delete q;
 }
+
 
 void AVLTree::DelMin(TreePointer &q, TreePointer &r, bool &h) {
     if (r->LeftNode != NULL) {
         DelMin(q, r->LeftNode, h);
         if (h) {balanceL(r,h);}
-        return
+        return;
     }
     q->Entry = r->Entry;
     q->count = r->count;
