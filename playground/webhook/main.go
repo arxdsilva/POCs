def12ad8e9ea1d7262b569f3e91a628c1dd6c86e
Arthur Silva <arxdsilva@gmail.com>
2019-11-12 15:45:30 -0300

    p/w: sign req

--- playground/webhook/main.go
+++ playground/webhook/main.go
@@ -2,13 +2,24 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+	"strings"
 
 	"github.com/arxdsilva/webhook"
 )
 
 func main() {
 	secret := []byte("f0247600-0574-11ea-8d71-362b9e155667")
-	body := []byte("a message")
-	sig := webhook.SignBody(secret, body)
-	fmt.Printf("signature: %x", sig)
+	body := "a message"
+	sig := webhook.SignBody(secret, []byte(body))
+	signature := fmt.Sprintf("sha1=%x", sig)
+	req, _ := http.NewRequest("POST", "", strings.NewReader(body))
+	req.Header.Add("x-hub-signature", signature)
+	h, err := webhook.Parse(secret, req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("signed: ", h.SignedBy(secret))
+	fmt.Println("Signature: ", h.Signature)
 }
